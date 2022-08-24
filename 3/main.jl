ef1e83bcee40f2d111a66a7f0e9fdd81d06e7e05
vinicius carlette <102614945+Vinicius1210@users.noreply.github.com>
2022-08-24 17:23:25 +0000

    Aula 3

--- 3/main.jl
+++ 3/main.jl
@@ -1,8 +1,26 @@
-f1(x)= sqrt(x)-cos(x)
+f1(x)= cos(x)-sqrt(x)
 f2(x)= 5*sin(x^2)-MathConstants.e^(x/10)
-f3(x)= 
+i(x) = ir*(MathConstants.e^((x*q)/(k*t)) - 1)
+g(x,V,R) = R*i(x) + x - V
 
-error = 10^-5
+f4(x) = g(x, 30, 1)
+f5(x) = g(x, 3, 1)
+f6(x) = g(x, 3, 10)
+f7(x) = g(x, (300*10^-3), 1)
+f8(x) = g(x, ((-300)*10^-3), 1)
+f9(x) = g(x, -30, 1)
+f10(x) = g(x, -30, 10)
+
+
+ir = 10^-12
+t = 300
+k = 1.380649*(10^-23)
+q = 1.60217663*(10^-19)
+
+
+
+error = 10^-3
+
 
 function bisection(a,b,f)
   x=(a+b)/2
@@ -23,6 +41,23 @@ println(r)
 r = bisection(0,1,f2)
 println(r)
 
-r = bisection(0,1,f3)
+r = bisection(0.8,0.9,f4)
 println(r)
-  
+
+r = bisection(0.7,0.8,f5)
+println(r)
+
+r = bisection(0.6,0.7,f6)
+println(r)
+
+r = bisection(0.29,0.31,f7)
+println(r)
+
+r = bisection(-0.29,-0.31,f8)
+println(r)
+
+r = bisection(-29.9,-30.1,f9)
+println(r)
+
+r = bisection(-29.9,-30.1,f10)
+println(r)
